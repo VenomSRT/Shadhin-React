202d76cc58bcd14dd27ae15984b48275af5a2c0f
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-11-20 01:27:10 +0200

    done

--- src/components/SidebarRight/Search/Search.js
+++ src/components/SidebarRight/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Search.css';
 import { getData } from '../../../api/api';
 
@@ -6,15 +6,61 @@ export const Search = () => {
   const [ data, setData ] = useState([])
   const [ input, setInput ] = useState('');
 
-  function filter(e) {
-    setInput(e.target.value);
+  const debounce = (func, wait) => {
+    let timeout;
+  
+    return function executedFunction(event) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(event);
+      };
+  
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  };
 
-    if(input.length >= 3) {
-      getData().then(response => {
-        console.log(response.data);
-      });
-    }
+  const delayFunction = debounce(handleInput, 1000);
+
+  function handleInput(e) {
+    setInput(e.target.value.trimLeft().toLowerCase());
+    setData(['Searching...'])
   }
+
+  useEffect(() => {
+    if (input.length) {
+      filter(input);
+    } else {
+      setData([]);
+    }   
+  }, [input]);
+
+  function filter(currentInput) {
+    getData().then(response => {
+      let filtered = [];
+
+      for(const key in response.data) {
+        filtered = filtered.concat(response.data[key].data.filter(item => {
+          return item.title.toLowerCase().includes(currentInput) || item.Artist.toLowerCase().includes(currentInput)
+        }));
+      }
+
+      if (filtered.length) {
+        setData(filtered);
+      } else {
+        setData(['No matches :(']);
+      }
+      
+    });
+  }
+
+  function reset(e) {
+    e.preventDefault();
+    setData([]);
+    e.target.parentElement.parentElement.firstElementChild.value = '';
+  }
+
+  console.log(data);
   
 
 
@@ -26,14 +72,17 @@ export const Search = () => {
             type="text"
             name=""
             placeholder="Search artist, songs, videos..."
-            onChange={filter}
+            onChange={(e) => delayFunction(e)}
           />
 
           <button type="submit" className="btn-submit">
             <i className="fa fa-search"></i>
           </button>
 
-          <button className="search-cancil">
+          <button
+            className="search-cancil"
+            onClick={reset}
+          >
             <img src='../img/icon-cross.png' alt=""/>
           </button>
         </form>
@@ -41,20 +90,39 @@ export const Search = () => {
 
       {data.length && (
         <div className="search-reasult">
+          {typeof data[0] !== 'string' && (
           <ul>
-            {data.forEach(elem => {
-              <li>
+            {data.map(elem => (
+              <li key={`${Math.random()}`}>
                 <a href="#!">
-                  <img src={(/<$size$>/g).test(elem.image) ? elem.image.replace((/<$size$>/), '300') : elem.image } alt=""/>
-                  <span>Habib wa</span>hid - Prithibir Joto Shukh
+                  <img src={
+                    elem.image.includes('<$size$>') ?
+                      elem.image.replace(/<\$size\$>/, '300') :
+                      elem.image
+                  } alt=""/>
+
+                  <span dangerouslySetInnerHTML={{
+                    __html: elem.title.toLowerCase().includes(input) ?
+                      elem.title.replace(new RegExp(input, 'i'),
+                      `<span style="opacity: 0.5">${elem.title.match(new RegExp(input, 'i'))}</span>`) :
+                      elem.Artist.replace(new RegExp(input, 'i'),
+                      `<span style="opacity: 0.5">${elem.Artist.match(new RegExp(input, 'i'))}</span>`)
+                  }}/>
                 </a>
               </li>
-            })}
+            ))}
           </ul>
+          )}
+
+          {data[0] === 'Searching...' && (
+            <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+          )}
+
+          {data[0] === 'No matches :(' && (
+            <span>{data[0]}</span>
+          )}
         </div>
       )}
-
-      
     </>
   )
 }
