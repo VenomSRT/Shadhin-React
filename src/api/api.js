14732f75220f775c5dcb246725d8aeeef5b4db78
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-11-20 19:08:39 +0200

    base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://43.240.103.34/api.shadhin/api/search?keyword=valo';
+//const BASE_URL = 'http://43.240.103.34/api.shadhin/api/search?keyword=valo';
+const BASE_URL = 'https://github.com/VenomSRT/Shadhin-React/blob/develop/src/api/data.json';
 
 export async function getData() {
   const response = await fetch(BASE_URL);
